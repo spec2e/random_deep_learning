c375463b571db99b1c1b636a3554fe674d51dbf9
spec2e <speconsult@gmail.com>
2017-07-28 13:48:57 +0200

    init

--- dqn/breakout.py
+++ dqn/breakout.py
@@ -27,7 +27,7 @@ class DQNAgent:
         self.memory = deque(maxlen=4000)
         self.gamma = 0.95  # discount rate
         self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
+        self.epsilon_min = 0.3
         self.epsilon_decay = 0.001
         self.learning_rate = 0.001
         self.model = self._build_model()
