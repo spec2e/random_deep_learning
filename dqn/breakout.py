287db222ee5065ebea4bc1127b51c99f938505a0
spec2e <speconsult@gmail.com>
2017-08-01 14:31:12 +0200

    breakout

--- dqn/breakout.py
+++ dqn/breakout.py
@@ -265,7 +265,8 @@ def process_observation(observation):
 
     processed_observation = np.array(img)
     assert processed_observation.shape == INPUT_SHAPE
-    return processed_observation.astype('uint8')  # saves storage in experience memory
+    to_type = processed_observation.astype('uint8')  # saves storage in experience memory
+    return to_type / 255
 
 
 def play_game():
